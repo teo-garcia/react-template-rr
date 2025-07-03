6fdea2104b0ae15ca0636f476a9a5ba9a2f4ecd8
teo-garcia <juanmateogarcia96@gmail.com>
2025-07-03 10:17:47 -0500

    refactor: improve @teo-garcia/tsconfig-shared

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -19,7 +19,7 @@ export async function loader() {
 export default function HomePage() {
   return (
     <section className='flex h-screen flex-col items-center justify-center gap-y-16'>
-      <Zap className='size-48 text-foreground dark:text-background lg:size-56 xl:size-72' />
+      <Zap className='size-48 text-primary lg:size-56 xl:size-72' />
     </section>
   )
 }
--- eslint.config.mjs
+++
@@ -1,4 +0,0 @@
-import base from '@teo-garcia/eslint-config-shared/base'
-import react from '@teo-garcia/eslint-config-shared/react'
-
-export default [...base, ...react]

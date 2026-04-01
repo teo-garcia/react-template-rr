64b7cfe5d7c556ec2d7db03ce7d0638ef3752522
teo-garcia <juanmateogarcia96@gmail.com>
2026-04-01 15:33:02 -0500

    refactor: adopt defineConfig syntax

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
+import { defineConfig } from 'eslint/config'
 import shared from '@teo-garcia/eslint-config-shared'
 import sharedPlaywright from '@teo-garcia/eslint-config-shared/playwright'
 import sharedReact from '@teo-garcia/eslint-config-shared/react'
 
-export default [...shared, ...sharedReact, ...sharedPlaywright]
+export default defineConfig([...shared, ...sharedReact, ...sharedPlaywright])

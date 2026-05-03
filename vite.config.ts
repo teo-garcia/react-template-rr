9c885b8d9ed88ba2fb73edfc46d8a278a46d85dc
teo-garcia <juanmateogarcia96@gmail.com>
2026-05-03 18:55:29 -0500

    fix: keep eslint out of vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { reactRouter } from '@react-router/dev/vite'
 import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
-// @ts-expect-error vite-plugin-eslint types are not aligned with Vite yet
-import eslint from 'vite-plugin-eslint'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 const shouldAnalyze = process.env.ANALYZE === 'true'
@@ -10,7 +8,6 @@ const shouldAnalyze = process.env.ANALYZE === 'true'
 export default defineConfig({
   plugins: [
     reactRouter(),
-    eslint(),
     tsconfigPaths(),
     shouldAnalyze &&
       visualizer({

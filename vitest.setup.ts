7515620a05435106e092635cff5cd1cc84763b25
teo-garcia <juanmateogarcia96@gmail.com>
2026-05-07 09:53:08 -0500

    fix: align frontend runtime health checks

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,21 @@
 import './app/app.css'
+
+import { afterAll, afterEach, beforeAll } from 'vitest'
+
+let server: typeof import('./app/lib/mocks/node').server | undefined
+
+if (!('document' in globalThis)) {
+  beforeAll(async () => {
+    const mswNode = await import('./app/lib/mocks/node')
+    server = mswNode.server
+    server.listen({ onUnhandledRequest: 'bypass' })
+  })
+
+  afterEach(() => {
+    server?.resetHandlers()
+  })
+
+  afterAll(() => {
+    server?.close()
+  })
+}
